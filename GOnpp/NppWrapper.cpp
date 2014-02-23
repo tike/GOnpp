0deb98c68c140f54a8454cec3f96d99ce5316957
tike <timflex@gmx.de>
2014-02-23 06:53:36 +0100

    added content of fileutils.cpp to NppWrapper

--- GOnpp/NppWrapper.cpp
+++ GOnpp/NppWrapper.cpp
@@ -11,3 +11,55 @@ LRESULT NppWrapper::send_scintilla(UINT msg, WPARAM wparam, LPARAM lparam)
 {
 	return ::SendMessage(_npp._scintillaMainHandle, msg, wparam, lparam);
 }
+
+// checks the current files extention and compares it to ".go"
+// returns TRUE on match, FALSE otherwise
+BOOL NppWrapper::current_file_is_go_file(void){
+	TCHAR ext[MAX_PATH];
+	
+	// Check wether current file is a .go file
+	::SendMessage(_npp._nppHandle, NPPM_GETEXTPART, 0, (LPARAM)ext);
+	if (_tcscmp(ext, TEXT(".go")) != 0){
+		return FALSE;
+	}
+	return TRUE;
+}
+
+// reloads all open files
+// TODO: make this check the extension and only reload .go files
+BOOL NppWrapper::reload_all_files(void){
+	int num_files = ::SendMessage(_npp._nppHandle, NPPM_GETNBOPENFILES, 0, ALL_OPEN_FILES);
+		
+	TCHAR **file_names = (TCHAR**) calloc(num_files, sizeof(TCHAR*));
+	if (file_names == NULL) return FALSE;
+
+	int i = 0;
+	for (i=0; i<num_files; i++){
+		file_names[i] = (TCHAR*) calloc(MAX_PATH, sizeof(TCHAR));
+		if (file_names[i] == NULL){
+			int j = 0;
+			for (j=0; j<i; j++){
+				free(file_names[j]);
+			}
+			free(file_names);
+			return FALSE;
+		}
+	}
+	if ( num_files != ::SendMessage(_npp._nppHandle, NPPM_GETOPENFILENAMES, (WPARAM) file_names, num_files)){
+		return FALSE;
+	}
+
+		
+	for ( i=0; i<num_files; i++){
+		TCHAR curr_file[MAX_PATH];
+		_tcsncpy(curr_file, file_names[i], MAX_PATH);
+		::SendMessage(_npp._nppHandle, NPPM_RELOADFILE, FALSE, (LPARAM) curr_file);
+	}
+		
+	for (i=0; i<num_files; i++){
+		free(file_names[i]);
+	}
+	free(file_names);
+
+	return TRUE;
+}
--- GOnpp/NppWrapper.h
+++ GOnpp/NppWrapper.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "PluginInterface.h"
+#include "tstring.h"
 
 class NppWrapper
 {
@@ -8,6 +9,9 @@ public:
 	NppWrapper(const NppData);
 
 	LRESULT send_scintilla(UINT msg, WPARAM, LPARAM);
+
+	BOOL current_file_is_go_file(void);
+	BOOL reload_all_files(void);
 
 private:
 	const NppData _npp;
